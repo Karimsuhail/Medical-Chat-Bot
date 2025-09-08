cce356d5d1d8d8b1b251819e39c1873d62fa7c66
KarimSuhail <karimsuhaildev@gmail.com>
2025-09-08 14:22:53 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="KARIM SUHAIL SHANULLA",
+    author_email="karimsuhaildev@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
